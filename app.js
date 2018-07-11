381994e668c765dcd6d05917bfb006ee55dc0d1f
Heather Park <hthr.prk@gmail.com>
2018-07-11 14:21:57 -0700

    Implement attack function

--- app.js
+++ app.js
@@ -5,5 +5,11 @@ new Vue({
       player: 100,
       monster: 100
     }
+  },
+  methods: {
+    attack: function() {
+      this.scores.player -= 5;
+      this.scores.monster -= 9;
+    }
   }
 });
