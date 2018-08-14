64f0c9bba70548e324783918f853ecb072b7c9d9
Heather Park <hthr.prk@gmail.com>
2018-08-14 12:51:40 -0700

    Implement new game function

--- app.js
+++ app.js
@@ -1,15 +1,48 @@
 new Vue({
   el: '#app',
   data: {
-    scores: {
+    hp: {
       player: 100,
       monster: 100
+    },
+    damage: {
+      attack: {
+        player: 5,
+        monster: 9
+      },
+      specialAttack: {
+        player: 9,
+        monster: 15
+      }
     }
   },
   methods: {
     attack: function() {
-      this.scores.player -= 5;
-      this.scores.monster -= 9;
+      if (this.hp.player && this.hp.monster
+        || this.hp.player >= 0 && !this.hp.monster 
+        || this.hp.monster >= 0 && !this.hp.player) {
+        this.updateHP('player', 'attack');
+        this.updateHP('monster', 'attack');
+      }
+    },
+    newGame: function() {
+      this.hp.player = 100;
+      this.hp.monster = 100;
+    },
+    updateHP: function(character, attackType) {
+      if (this.hp[character] && this.damage[attackType]) {
+        this.hp[character] -  this.damage[attackType][character] >= 0
+          ? this.hp[character] -= this.damage[attackType][character]
+          : this.hp[character] = 0;
+      }
+    },
+    specialAttack: function() {
+      if (this.hp.player && this.hp.monster
+        || this.hp.player >= 0 && !this.hp.monster 
+        || this.hp.monster >= 0 && !this.hp.player) {
+        this.updateHP('player', 'specialAttack');
+        this.updateHP('monster', 'specialAttack');
+      }
     }
   }
 });
